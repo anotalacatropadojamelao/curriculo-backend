68537c91224c2388a051d3b42d64d6454f2a24de
anotalacatropadojamelao <cauaperezrj@gmail.com>
2026-05-04 20:42:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,8 @@
 const express = require("express");
-const { createClient } = require("@supabase/supabase-js");
-
 const app = express();
-app.use(express.json());
 
-const supabase = createClient(
-  "https://wvewdjbaalgoybicqbjv.supabase.co",
-eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind2ZXdkamJhYWxnb3liaWNxYmp2Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3Nzg0ODY0NCwiZXhwIjoyMDkzNDI0NjQ0fQ.qk8nN7sUOkCrVBbUSYYhoabRbTAFjUGivLgNejRF2lg
-);
-
-app.post("/webhook", async (req, res) => {
-  const data = req.body;
-
-  if (data.status === "paid") {
-    const email = data.customer.email;
-
-    try {
-      // cria usuário no auth
-      await supabase.auth.admin.createUser({
-        email: email,
-        email_confirm: true
-      });
-
-      // salva no banco
-      await supabase.from("usuarios").insert({
-        email: email,
-        acesso: true
-      });
-
-      console.log("Usuário criado:", email);
-
-    } catch (err) {
-      console.error("Erro:", err);
-    }
-  }
-
-  res.sendStatus(200);
+app.get("/", (req, res) => {
+  res.send("Servidor funcionando");
 });
 
 app.listen(3000, () => {
